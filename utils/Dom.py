a4142b70d3f5ae4fbae39ea33d48e3c4b3257b50
issamoxix <issamhaidaoui@gmail.com>
2021-10-07 08:23:46 +0100

    added try

--- utils/Dom.py
+++ utils/Dom.py
@@ -4,7 +4,11 @@ class Dom:
         self.url = url
         self.s = HTMLSession()
     def GetDom(self):
-        r = self.s.get(self.url)
-        r.html.render(timeout=20)
-        return r.text
+        try:
+            r = self.s.get(self.url)
+            r.html.render(timeout=20)
+            return r.text
+        except:
+            self.s.close()
+            return print('Erorr in the Dom class')
 
